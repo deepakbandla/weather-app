60d925f1e389221cdc1e27681859c243d887f663
Deepak Bandla <deepakbandla4567@gmail.com>
2025-07-07 16:25:17 +0000

    heading and fetching data from api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
 
+fetch('https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=jgeLK7u6TzMYsNp5HiGTgR9AJJpWllRi')
+    .then(response =>{
+        return response.json()
+    })
+    .then(response =>{
+        console.log(response)
+    })
